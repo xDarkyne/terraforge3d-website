49b5547933619c85bd9ad6e7f66f87c8346abd15
xDarkyne <darkyne.gaming@outlook.de>
2022-03-24 13:40:06 +0100

    feat: implemented hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import 'styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { MainLayout } from 'components/layouts';
+import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(2765308, 6);
+  }, []);
+
   return (
     <MainLayout>
       <Component {...pageProps} />
