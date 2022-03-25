d4505591e6c8040d832d09e4387f7a3dd09d5b0b
xDarkyne <darkyne.gaming@outlook.de>
2022-03-25 19:33:31 +0100

    feat: added basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/terraforge3d-website',
   poweredByHeader: false,
   images: {
     loader: 'akamai',
