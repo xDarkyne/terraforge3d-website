340f6fe6b08e28d112c43fdfe0ed28001533fc63
xDarkyne <darkyne.gaming@outlook.de>
2022-03-27 17:21:45 +0200

    feat: added contributor data

---
+++ src/values/contributors.ts
@@ -0,0 +1,22 @@
+/**
+ * This interface is just for type-safety and a better developer-experience.
+ */
+export interface IContributor {
+  name: string;
+  avatar: string;
+  githubUrl: string;
+  extraUrl: string;
+}
+
+/**
+ * Array containing data of the contributors displayed on the homepage.
+ * Add/Remove contributors here.
+ */
+export const contributors: IContributor[] = [
+  {
+    name: 'Darkyne',
+    avatar: 'some avatar url',
+    githubUrl: 'github url',
+    extraUrl: 'any custom url like personal website, etc',
+  },
+];
